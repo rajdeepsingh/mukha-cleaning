d6ff917ead436b0b84e5fb2ca59f5741b2184ada
Rajdeep Singh <rajdeepsingh@live.in>
2017-04-16 17:36:44 -0500

    added smne interactivity to the hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,16 @@
 // https://jsfiddle.net/Guruprasad_Rao/xya6gs93/1/
 
-$(function() {
-	var item = "This is an item";
-	var persons = [];
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
 
-	var person = {
-		"name": "John Doe",
-		"drink": "Latte"
-	};
+        var target = this.hash;
+        $target = $(target);
 
-	persons.push(person);
-
-	person = {
-		"name": "Jane Smith",
-		"drink": "Machiatto"
-	};
-
-	alert("This is an alert");
-	persons.push(person);
-
-	console.log("This should be working now");
-
-	$.each(persons, function(i, person) {
-		console.log(person.name + ' and they would like a ' + person.drink);
-		console.log(person);
-	});
-
-	function addList() {
-	var item = "<ul>"+
-	"<li><a>Link 1</a></li>"+
-	"<li><a>Link 2</a></li>"+
-	"<li><a>Link 3</a></li>"+
-	"<li><a>Link 4</a></li>"+
-	"<li><a>Link 5</a></li>"+
-	"</ul>"
-
-	$('.list-please').html(item);
-}
-
-addList();
-
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
