6fbc44d3fdd041444f3d04e5ec69c091169bdd3d
Rajdeep Singh <rajdeepsingh@live.in>
2017-04-15 23:43:30 -0500

    added an extra list in the footer with javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ $(function() {
 		"drink": "Machiatto"
 	};
 
+	alert("This is an alert");
 	persons.push(person);
 
 	console.log("This should be working now");
@@ -24,4 +25,19 @@ $(function() {
 		console.log(person.name + ' and they would like a ' + person.drink);
 		console.log(person);
 	});
+
+	function addList() {
+	var item = "<ul>"+
+	"<li><a>Link 1</a></li>"+
+	"<li><a>Link 2</a></li>"+
+	"<li><a>Link 3</a></li>"+
+	"<li><a>Link 4</a></li>"+
+	"<li><a>Link 5</a></li>"+
+	"</ul>"
+
+	$('.list-please').html(item);
+}
+
+addList();
+
 });
